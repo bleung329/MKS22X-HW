3b28067e08e217e2a98dd5432292cce5e5df69c2
bleung329 <brianleung329@gmail.com>
2017-02-06 12:57:23 -0500

    woo

--- HW01/Recursion.java
+++ HW01/Recursion.java
@@ -2,7 +2,14 @@ public class Recursion{
 	public static String name(){
 		return "Leung,Brian";
 	}
+	public static boolean equal(double a, double b){
+		
+	}
 	public static double sqrt(double n){
 		double guess = 1;
+		double better_guess;
+	}
+	public static void main(String[] noargs){
+		System.out.println(sqrt(100));
 	}
 }
