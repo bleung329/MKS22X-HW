462c1808245cbf1152b2e4dc2e186fe66e803660
Brian Leung <brianleung329@gmail.com>
2017-02-08 00:18:12 -0500

    AAAAAAHHH IT WORKED

--- HW01/Recursion.java
+++ HW01/Recursion.java
@@ -3,13 +3,21 @@ public class Recursion{
 		return "Leung,Brian";
 	}
 	public static boolean equal(double a, double b){
-		
+		return ((a/b >= 0.99999)&&(a/b <= 1.00001));
 	}
 	public static double sqrt(double n){
-		double guess = 1;
-		double better_guess;
+		return sqrti(n,1);
 	}
-	public static void main(String[] noargs){
-		System.out.println(sqrt(100));
+	public static double sqrti(double n,double guess){
+		if (equal(guess*guess,n)){
+			return guess;
+		}else{
+			double better_guess = ( n / guess + guess) / 2;
+			return sqrti(n,better_guess);
+		}
 	}
+	/*public static void main(String[] noargs){
+		System.out.println(sqrt(169));
+		System.out.println(name());
+	}*/
 }
