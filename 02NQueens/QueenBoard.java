45be17f55ed388125dd48eade8447ed48be41804
Brian Leung <brianleung329@gmail.com>
2017-02-13 23:47:53 -0500

    put queen!

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -11,8 +11,8 @@ public class QueenBoard{
 		board[2][1] = 13;
 		return true;
 		//return solveH(n);
-	}
-	/*private boolean solveH(int n){
+	}/*
+	private boolean solveH(int n){
 		for (int i=0;i<board.length;i++){
 			if (board[n][i] == 1){
 				continue;
@@ -20,7 +20,7 @@ public class QueenBoard{
 			else{
 				
 		}
-	}/*
+	}
 	public boolean countSol(){
 	}
 	public String getCount(){
@@ -30,19 +30,27 @@ public class QueenBoard{
 		for (int i = 0; i<board.length; i++){
 			board[x][i] = 1;
 			board[i][y] = 1;
+			if (!(x-i<0||y-i<0)){
+			board[x-i][y-i] = 1;}
+			if (!(x+i>board.length-1||y+i>board.length-1)){
+			board[x+i][y+i] = 1;}
+			if (!(x+i>board.length-1||y-i<0)){
+			board[x+i][y-i] = 1;}
+			if (!(x-i<0||y+i>board.length-1)){
+			board[x-i][y+i] = 1;}
 		}
 		board[x][y] = 2;
 	}
 	public String board(){
 		String ret = "";
-		for (int i = 0; i<this.board.length; i++){ret+=Arrays.toString(this.board[i]);}
+		for (int i = 0; i<this.board.length; i++){ret+=Arrays.toString(this.board[i])+"\n";}
 		return ret;
 	}
 	public static void main(String[] args){
 		QueenBoard slay = new QueenBoard();
 		System.out.println(slay.solve(6));
-		slay.putQueen(2,1);
-		System.out.println(slay.board(s));
+		slay.putQueen(3,2);
+		System.out.println(slay.board());
 
 	}
 }
