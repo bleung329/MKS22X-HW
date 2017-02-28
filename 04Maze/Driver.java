20b4af91ccc2f54d4644decfa7ac96abe44c4338
bleung329 <brianleung329@gmail.com>
2017-02-28 12:38:21 -0500

    progress on maze I hope i can finish it T_T

--- 04Maze/Driver.java
+++ 04Maze/Driver.java
@@ -2,10 +2,8 @@ public class Driver{
 
     public static void main(String[]args){
         Maze f;
-	try{
-	    f = new Maze("data1.dat");//true animates the maze.
-        }catch(Exception e){
-	}/*
+	f = new Maze("data1.dat");//true animates the maze.
+	/*
         f.clearTerminal();
         f.setAnimate(true);
         f.solve();
