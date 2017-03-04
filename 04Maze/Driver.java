c2d8539d547d5c99f4dbc8197f5754a8465cffcd
bleung329 <brianleung329@gmail.com>
2017-03-04 02:57:38 -0500

    replace the ctry value later

--- 04Maze/Driver.java
+++ 04Maze/Driver.java
@@ -2,7 +2,7 @@ public class Driver{
 
     public static void main(String[]args){
         Maze f;
-	f = new Maze("data2	.dat");
+	f = new Maze("data2.dat");
 	f.boord();
 	f.solve();
 	f.boord();//true animates the maze.
