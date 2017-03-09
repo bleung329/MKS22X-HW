a366138201e0b6c15693fa6f908585068a8bd951
bleung329 <brianleung329@gmail.com>
2017-03-09 14:10:45 -0500

    added 06SafetyPinToTheJugular

---
+++ 06SafetyPinToTheJugular/Quiz2Redux.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class Quiz2Redux{
+	public static ArrayList<String> combinations(String s){
+		Arraylist<String>words = new ArrayList<String>();
+		combinationH(s,words,s.length()-1);
+		return words;
+	}
+	
+}
