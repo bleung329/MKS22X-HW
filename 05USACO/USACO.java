c5ddb23b608756788121cd58c917d0dc935318c7
bleung329 <brianleung329@gmail.com>
2017-03-09 05:37:47 -0500

    yep

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -2,7 +2,10 @@ public class USACO{
     //Here is the bronze problem, Cows make a lake
     public int bronze(String filename){
     }
+    
     //This is the end of the bronze problem code
     //Here is the silver
-    
+    public int silver(String filename){
+    }
+    //End of the silver
 }
