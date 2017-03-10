69ddc6abeb68e7e133c53b8c1f228da4becd1652
bleung329 <brianleung329@gmail.com>
2017-03-09 23:17:48 -0500

    started off

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,10 +1,24 @@
+import java.util.ArrayList;
+
+
 public class USACO{
-    //Here is the bronze problem, Cows make a lake
+    //Here is the bronze problem, Cows make a lake by stomping
     public int bronze(String filename){
+		//mr K why are you so smart
+		//Big thanks to mr K for the scanner, try catch thing
+		Scanner in1 = null;
+		try{
+			File file = new File(filename);
+			in1 = new Scanner(file);
+		}
+		catch(Exception e){
+			System.out.println("Something went wrong. Exiting...");
+			System.exit(0);
+		}
     }
     
     //This is the end of the bronze problem code
-    //Here is the silver
+    //Here is the silver problem
     public int silver(String filename){
     }
     //End of the silver
