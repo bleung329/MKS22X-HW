c379345017fd975b317d79a18c4441bf191ce3fa
bleung329 <brianleung329@gmail.com>
2017-03-16 12:28:50 -0400

    Shouldve done this a while ago

--- 06SafetyPinToTheJugular/Quiz2Redux.java
+++ 06SafetyPinToTheJugular/Quiz2Redux.java
@@ -1,9 +1,32 @@
- import java.util.ArrayList;
+import java.util.ArrayList;
 public class Quiz2Redux{
 	public static ArrayList<String> combinations(String s){
-		Arraylist<String>words = new ArrayList<String>();
-		combinationH(s,words,s.length()-1);
+		ArrayList<String>words = new ArrayList<String>();
+		combinationH(s,words,s.length());
 		return words;
 	}
+	public static void combinationH(String s, ArrayList<String> words, int position)
+	{
+		int placehold = words.size();
+		if(position<0){return;}
+		if (words.size() == 0){
+			words.add("");
+		}
+		else{
+			words.add(""+s.charAt(position));
+			for (int i = 1; i<placehold; i++){
+				words.add(words.get(i) + words.get(placehold));
+			}
+			
+		}
+		combinationH(s,words,position-1);
+	}
+	public static void p(Object a){
+		System.out.println(a);
+	}
+	public static void main(String[] args){
+		System.out.println(Quiz2Redux.combinations("abc"));
+	}
+	
 	
 }
