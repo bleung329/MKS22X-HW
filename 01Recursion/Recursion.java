2b62da0ef2e56e3feebc484348ba3e507aa00554
bleung329 <brianleung329@gmail.com>
2017-03-20 12:26:24 -0400

    Whoops i forgot 0

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -3,24 +3,29 @@ public class Recursion{
 		return "Leung,Brian";
 	}
 	public static boolean equal(double a, double b){
-		return ((a/b >= 0.99999)&&(a/b <= 1.00001));
+		return ((a/b >= 0.999999999)&&(a/b <= 1.0000001));
 	}
 	public static double sqrt(double n){
+		//System.out.println("Testing "+n);
 		return sqrti(n,1);
 	}
 	public static double sqrti(double n,double guess){
 		if (n<0){
 			throw new IllegalArgumentException();
 		}
+		if (n==0){
+			return 0;
+		}
 		if (equal(guess*guess,n)){
 			return guess;
 		}else{
-			return sqrti(n,( n / guess + guess) / 2);
+			return sqrti(n,(n / guess + guess)/ 2);
 		}
-	}/*
+	}
 	public static void main(String[] noargs){
-		System.out.println(sqrt(169));
-		System.out.println(name());
+		System.out.println(sqrt(1024));
+		//System.out.println(name());
 		System.out.println(sqrt(-1));
-	}*/
+	}
 }
+// In 6 and 15
