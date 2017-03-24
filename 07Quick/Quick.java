46573794125e3cdc1d968021d3d01af071d5a10a
bleung329 <brianleung329@gmail.com>
2017-03-24 10:32:11 -0400

    Done with the merge()

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -104,11 +104,18 @@ public class Quick
 		int p2 = 0;
 		int[] retArr = new int[arr1.length+arr2.length];
 		int out = -1;
-		for (int i = 0; i<retArr.length-1; i++){
-			printArr(retArr);
-			System.out.println("p1 = "+p1+"\np2 = "+p2);	
-			if ((p2 >= arr2.length)||(p1 >= arr1.length)){
+		int longarr = 0;
+		for (int i = 0; i<retArr.length; i++){
+			//printArr(retArr);
+			//System.out.println("p1 = "+p1+"\np2 = "+p2);	
+			if ((p1 >= arr1.length)){
 				out = i;
+				longarr = 2;
+				break;
+			}
+			if ((p2 >= arr2.length)){
+				out = i;
+				longarr = 1;
 				break;
 			}
 			if (arr1[p1]>=arr2[p2]){
@@ -119,8 +126,18 @@ public class Quick
 				p1++;
 			}
 		}
-		
-		printArr(retArr);
+		if (out!=-1){
+			for (int i = out; i<retArr.length; i++){
+				if (longarr == 2){
+					retArr[i] = arr2[p2];
+					p2++;
+				}else{
+					retArr[i] = arr1[p1];
+					p1++;
+				}
+			}
+		}
+		//printArr(retArr);
 		return retArr;
 	}
 	//END MERGESORT
@@ -131,8 +148,8 @@ public class Quick
 		//int[]ary = Quick.randArray(500,100);
 		//Quick.printArr(dutchyH(ary,0,ary.length-1,0));
 		//Quick.printArr(Quick.sortit(ary, 0, ary.length - 1));
-		int[] ar1 = {1,5,7,9,14};
-		int[] ar2 = {3,6,9,20,100};
+		int[] ar1 = {1,5,7};
+		int[] ar2 = {10,10,10,10,10};
 		Quick.printArr(Quick.merge(ar1,ar2));
     }
 	
