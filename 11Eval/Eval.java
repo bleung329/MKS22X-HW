5cd58069ad60d14f3baba49e8c6a0066767ef66a
bleung329 <brianleung329@gmail.com>
2017-04-20 13:20:33 -0400

    blargh

---
+++ 11Eval/Eval.java
@@ -0,0 +1,17 @@
+public class Eval
+{
+	public static double eval(String S)
+	{	
+	}
+	public static boolean isOp(String s)
+	{
+		return (s.equals("+")|s.equals("-")|s.equals("/")|s.equals("*")|s.equals("%"));
+	}
+	public static double apply(String A, String B, String OP)
+	{
+	}
+	public static void main(String[] args)
+	{
+		
+	}
+}
