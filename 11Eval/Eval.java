2edec084cd0b3b6c62f6b79c3c19d6f129851fa1
bleung329 <brianleung329@gmail.com>
2017-04-21 12:33:16 -0400

    Will continue

--- 11Eval/Eval.java
+++ 11Eval/Eval.java
@@ -1,8 +1,16 @@
+import java.util.Stack;
+
 public class Eval
 {
 	public static double eval(String S)
 	{
 		String[] s = S.split(" ");	
+		Stack<String> ops = new Stack<String>();
+		for (int i = s.length-1; i>-1; i--)
+		{
+			//System.out.println(s[i]);
+			ops.add(s[i]);
+		}
 		return 1.0;
 	}
 	public static boolean isOp(String s)
@@ -11,32 +19,27 @@ public class Eval
 	}
 	public static double apply(String A, String B, String OP)
 	{
-		if (isOp(OP))
+		double a = Double.parseDouble(A);
+		double b = Double.parseDouble(B);
+		switch (OP)
 		{
-			double a = Double.parseDouble(A);
-			double b = Double.parseDouble(B);
-			switch (OP)
-			{
-				case "+":
-					return (a+b);
-				case "-":
-					return (a-b);
-				case "/":
-					return (a/b);
-				case "*":
-					return (a*b);
-				case "%":
-					return (a%b);
-				default:
-					break;
-			}
+			case "+":
+				return (a+b);
+			case "-":
+				return (a-b);
+			case "/":
+				return (a/b);
+			case "*":
+				return (a*b);
+			case "%":
+				return (a%b);
+			default:
+				break;
 		}
-		
-		return -0.0;
-		
+		return -0.0;	
 	}
 	public static void main(String[] args)
 	{
-		
+		eval("1 2 3 + +");
 	}
 }
