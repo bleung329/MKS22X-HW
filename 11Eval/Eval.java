ddef635e586207d82946634c42be2f0e79e07c2b
bleung329 <brianleung329@gmail.com>
2017-04-21 11:44:02 -0400

    Howdy

--- 11Eval/Eval.java
+++ 11Eval/Eval.java
@@ -1,7 +1,9 @@
 public class Eval
 {
 	public static double eval(String S)
-	{	
+	{
+		String[] s = S.split(" ");	
+		return 1.0;
 	}
 	public static boolean isOp(String s)
 	{
@@ -9,6 +11,29 @@ public class Eval
 	}
 	public static double apply(String A, String B, String OP)
 	{
+		if (isOp(OP))
+		{
+			double a = Double.parseDouble(A);
+			double b = Double.parseDouble(B);
+			switch (OP)
+			{
+				case "+":
+					return (a+b);
+				case "-":
+					return (a-b);
+				case "/":
+					return (a/b);
+				case "*":
+					return (a*b);
+				case "%":
+					return (a%b);
+				default:
+					break;
+			}
+		}
+		
+		return -0.0;
+		
 	}
 	public static void main(String[] args)
 	{
