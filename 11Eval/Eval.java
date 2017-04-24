f6b875fe58703c340d65339b1a591eb796413faf
bleung329 <brianleung329@gmail.com>
2017-04-24 11:47:46 -0400

    hmmm

--- 11Eval/Eval.java
+++ 11Eval/Eval.java
@@ -1,5 +1,6 @@
 import java.util.Stack;
 import java.util.Arrays;
+import java.util.Queue;
 
 public class Eval
 {
@@ -9,6 +10,7 @@ public class Eval
 	}
 	public static double eval(String S)
 	{
+		//PARSE STRING
 		String[] s = S.split(" ");	
 		Stack<String> ops = new Stack<String>();
 		for (int i = s.length-1; i>-1; i--)
@@ -16,29 +18,40 @@ public class Eval
 			//System.out.println(s[i]);
 			ops.add(s[i]);
 		}
+		//END PARSE STRING
+		
 		String[] holder = new String[3];
 		String temp = "";
-		while (ops.size() > 1)
+		
+		for (int i = 0; i<3; i++)
 		{
-			for (int i = 0; i<holder.length; i++)
-			{
-				holder[i] = ops.pop();
-			}
-			p(ops);
-			System.out.println(Arrays.toString(holder));
+			holder[i] = ops.pop();
+		}
+		System.out.println(ops.size());
+		while (ops.size() > 0 && holder.length >= 0)
+		{
+			System.out.println("what");
 			if (isOp(holder[2]))
 			{
 				temp = Double.toString(apply(holder[0],holder[1],holder[2]));
 				ops.add(temp);
+				p(ops);
+				System.out.println("Holder is: "+Arrays.toString(holder));
 			}
 			else
 			{
-				for(String item: holder)
-				{
-					ops.add(item);
-				}
+				ops.add(holder[0]);
+				p(ops);
+				holder[0] = holder[1];
+				holder[1] = holder[2];
+				holder[2] = ops.pop();
 			}
+			p(ops); //print stuff here
+			System.out.println("Holder is: "+Arrays.toString(holder));
 		}
+		p(ops);
+		System.out.println("Holder is: "+Arrays.toString(holder));
+		ops.add(holder[0]);
 		return Double.parseDouble(ops.pop());
 	}
 	public static boolean isOp(String s)
@@ -69,6 +82,6 @@ public class Eval
 	}
 	public static void main(String[] args)
 	{
-		System.out.println(eval("1 2 3 + +"));
+		System.out.println(eval("2 3 +"));
 	}
 }
