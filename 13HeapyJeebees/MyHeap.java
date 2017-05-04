7feea078443f9473982db4c29ef8cb491449fe6b
bleung329 <brianleung329@gmail.com>
2017-05-04 12:25:25 -0400

    mm

--- 13HeapyJeebees/MyHeap.java
+++ 13HeapyJeebees/MyHeap.java
@@ -1,10 +1,24 @@
 public class MyHeap
 {
-	public add(String item)
-	{
-	}
-	public remove(String item)
-	{
-	}
-	public 
+    public MyHeap()
+    {
+    }
+    public MyHeap(boolean bool)
+    {
+    } 
+    public void add(String item)
+    {
+    }
+    public String remove(String item)
+    {
+    }
+    public String peek()
+    {
+    }
+    private void pushUp()
+    {
+    }
+    private void pushDown()
+    {
+    }
 }
