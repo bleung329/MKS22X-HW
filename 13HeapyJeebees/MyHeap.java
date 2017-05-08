d9167a35e4984c8f096ad53a3a6990397c5fc138
bleung329 <brianleung329@gmail.com>
2017-05-08 12:28:57 -0400

    uh oh

--- 13HeapyJeebees/MyHeap.java
+++ 13HeapyJeebees/MyHeap.java
@@ -1,24 +1,55 @@
+import java.util.ArrayList;
+
 public class MyHeap
 {
+	private ArrayList<String> heap;
+	private int style;
+	
     public MyHeap()
     {
+		heap = new ArrayList<String>();
+		heap.add(" ");
     }
+	
+	//True = Max
+	//False = Min
     public MyHeap(boolean bool)
     {
-    } 
+		heap = new ArrayList<String>();
+		if (bool)
+			style = 1;
+		else
+			style = 0;
+    }
+	
     public void add(String item)
     {
+		heap.set(heap.size(),item);
+		pushUp();
     }
+	
     public String remove(String item)
     {
     }
     public String peek()
     {
     }
+	
     private void pushUp()
     {
     }
     private void pushDown()
     {
     }
+	
+	private String dePrint()
+	{
+		String ret = "[";
+		for (String hop: heap)
+		{
+			ret+=hop+" "
+		}
+		ret+="]";
+		return ret;
+	}
 }
