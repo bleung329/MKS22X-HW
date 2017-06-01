c7c3cc891a1113718eaa8484ec84c8b98dbdf5d5
bleung329 <brianleung329@gmail.com>
2017-05-31 21:22:19 -0400

    wow i just dont read directions

--- 06SafetyPinToTheJugular/Quiz2Redux.java
+++ 06SafetyPinToTheJugular/Quiz2Redux.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 public class Quiz2Redux{
 	public static String name(){
 		return "Leung, Brian";
@@ -7,6 +8,11 @@ public class Quiz2Redux{
 	public static ArrayList<String> combinations(String s){
 		ArrayList<String>words = new ArrayList<String>();
 		combinationH(s,words,s.length());
+		for (int i = 0; i<words.size(); i++)
+		{
+			words.set(i, reverse(words.get(i)));
+		}
+		Collections.sort(words);
 		return words;
 	}
 	public static void combinationH(String s, ArrayList<String> words, int position)
@@ -25,8 +31,22 @@ public class Quiz2Redux{
 		combinationH(s,words,position-1);
 	}
 	
+	/*
+	heres a reverse function because apparently i've been placing the items in the wrong order
+	*/
+	private static String reverse(String str) 
+	{
+		String reverse = "";
+		int length = str.length();
+		for(int i = length - 1; i >= 0; i--) {
+			reverse = reverse + str.charAt(i);
+		}
+		return reverse;
+	}
+	/*
 	public static void main(String[] args){
 		System.out.println(Quiz2Redux.combinations("kji"));
 	}
+	*/
 	
 }
